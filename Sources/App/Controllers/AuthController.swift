0ab229e39a9581f814998a18fef49e2563f7347e
Alexander <a.a.grigoryev@icloud.com>
2022-07-09 20:15:35 +0300

    user response has been fixed

--- Sources/App/Controllers/AuthController.swift
+++ Sources/App/Controllers/AuthController.swift
@@ -8,16 +8,15 @@
 import Vapor
 
 class AuthController {
-    
-    let response = RegisterResponse(result: 1,
-                                    user_message: "User been succesfully registered")
-    
     func register(_ req: Request) throws -> EventLoopFuture<RegisterResponse> {
         guard let body = try? req.content.decode(RegisterRequest.self) else {
             throw Abort(.badRequest)
         }
         
         print(body)
+        
+        let response = RegisterResponse(result: 1,
+                                        user_message: "User been succesfully registered")
     
         return req.eventLoop.future(response)
     }
